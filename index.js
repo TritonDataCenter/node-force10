f3e02376f6a0bdc475225b011cabb516e15f448c
Joshua M. Clulow <jmc@joyent.com>
2018-06-08 23:26:45 +0000

    expose "explode_port_range"

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ var lib_ssh = require('./lib/ssh');
 module.exports = {
 	Force10ParserStream: lib_parser.Force10ParserStream,
 	Force10BasicParserStream: lib_basic_parser.Force10BasicParserStream,
-	Force10Manager: lib_ssh.Force10Manager
+	Force10Manager: lib_ssh.Force10Manager,
+	explode_port_range: explode_port_range,
 };
