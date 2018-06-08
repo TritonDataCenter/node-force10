3748d6da168970955fc1cfe0749ced0fa450d1ae
Joshua M. Clulow <jmc@joyent.com>
2018-06-08 23:13:41 +0000

    add a simpler parser which does less transformation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /* vim: set ts=8 sts=8 sw=8 noet: */
 
 var lib_parser = require('./lib/parser');
+var lib_basic_parser = require('./lib/basic_parser');
 var lib_ssh = require('./lib/ssh');
 
 module.exports = {
 	Force10ParserStream: lib_parser.Force10ParserStream,
+	Force10BasicParserStream: lib_basic_parser.Force10BasicParserStream,
 	Force10Manager: lib_ssh.Force10Manager
 };
